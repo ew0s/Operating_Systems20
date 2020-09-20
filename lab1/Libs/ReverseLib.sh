b1f9d791c0cb8d8005cf86ad160af950dae51eaf
Egor Savkovskiy <ewos365@mail.ru>
2020-09-20 03:46:52 +0300

    Final solution for ReverseLib.sh

--- lab1/Libs/ReverseLib.sh
+++ lab1/Libs/ReverseLib.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
+function _privateArgsExistReverse
+{
+    if ! [[ $1 -eq 2 ]]
+    then
+        echo "Error: reverse mode contains 2 arguments. Not $1" >> /dev/stderr
+        help_
+        exit -2
+    fi
+}
+
+function _privateIsCorrectFiles
+{
+    local fileToRead=$1
+    local fileToWrite=$2
+    if ! [[ -r $fileToRead ]]
+    then
+        echo "Error: permission denied for $fileToRead. Make sure you giving readable file."
+        help_
+        exit -8
+    fi
+
+    if ! [[ -w $fileToWrite ]]
+    then
+        echo "Error: permission denied for $fileToWrite. Make sure you giving writeable file."
+        help_
+        exit -9
+    fi
+}
+
+function _privateReadAndWrite
+{
+   tac $1 | rev 1> $2 2> /dev/stderr
+}
+
 function reverse
 {
-    if [[ -f $1 ]]
-    then
-        if [[ -f $2 ]]
-        then
-            echo "Work on it"
-        else
-            echo "Unable to initialize $2 file" >> /dev/stderr
-        fi
-    else
-        echo "Unable to initialize $1 file" >> /dev/stderr
-    fi
+    _privateArgsExistReverse $#
+    _privateIsCorrectFiles $1 $2
+    _privateReadAndWrite $1 $2
 }
