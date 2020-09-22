9b9659b22b958ff6799337b2fc5026acde41dd2b
Egor Savkovskiy <ewos365@mail.ru>
2020-09-22 18:10:10 +0300

    Pre-final solution for LogLib.sh

--- lab1/Libs/LogLib.sh
+++ lab1/Libs/LogLib.sh
@@ -13,12 +13,17 @@ function _privateLogSets
 {
     warnings="$(cat /var/log/anaconda/X.log | awk '{if(($3 == "(WW)") && ($1 == "["))print $0}')"
     information="$(cat /var/log/anaconda/X.log | awk '{if(($3 == "(II)") && ($1 == "["))print $0}')"
-    echo $warnings 
-    echo $information
+}
+
+function _privateShowColorLog
+{
+    echo -e "${warnings//"(WW)"/"\e[33mWarning:\e[0m"}"
+    echo -e "${information//"(II)"/"\e[94mInformation:\e[0m"}"
 }
 
 function log
 {
     _privateLogFilesExist
     _privateLogSets
+    _privateShowColorLog
 }
