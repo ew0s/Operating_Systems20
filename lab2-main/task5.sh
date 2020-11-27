57cdacb27d61431406914d5984eb6e00acc5925f
ew0s <ewos365@mail.ru>
2020-11-27 11:34:08 +0300

    Add: add 5 task to 2 main lab

--- lab2-main/task5.sh
+++ lab2-main/task5.sh
@@ -2,3 +2,11 @@
 
 bash task4.sh
 
+
+for line in $(cat /out/task4.out)
+do
+    currPPid=$(echo "$line" | awk '{print $3}')
+    currART=$(echo "$line" | awk '{print $4}')
+    
+    echo $currPPid $currART
+done
