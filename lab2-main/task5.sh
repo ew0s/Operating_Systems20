b253432c7e6faabfda1dfac88404cc962d98c991
ew0s <ewos365@mail.ru>
2020-11-27 10:25:17 +0300

    Add: add 5 task to 2 main lab

---
+++ lab2-main/task5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+bash task4.sh
+
+awk 'BEGIN {CurrLine=0;
+            LineCounter=0.0;
+            CurrPPID=0;
+            ARTSum=0.0}
+    {
+        if (CurrLine == 0)
+        {
+            CurrLine=$7
+            ARTSum+=$(11)
+            LineCounter=1.0;
+        }
+        else if (CurrPPID == $7)
+        {
+            ARTSum+=$(11)
+            LineCounter++;
+        }
+        else
+        {
+            print "Average_Sleeping_Children_of_ParentID=" CurrPPID " is " ARTSum/LineCounter
+            CurrPPID=$7
+            ARTSum=$(11)
+            LineCounter=1.0
+        }
+
+        print $0
+        CurrLine++;
+    }' out/task4.out > out/task5.out
