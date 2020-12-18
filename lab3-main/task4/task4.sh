a15a3b53e5093d94780bb048132d4ef3c83ebc5c
user <user@localhost.localdomain>
2020-12-18 14:17:06 +0300

    Fix: fixed 4 task

--- lab3-main/task4/task4.sh
+++ lab3-main/task4/task4.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sh ./task4_operation.sh&pid0=$!
-sh ./task4_operation.sh&pid1=$!
-sh ./task4_operation.sh&pid2=$!
+sh ./task4_operation.sh & pid1=$! 
+sh ./task4_operation.sh & pid2=$! 
+sh ./task4_operation.sh & pid3=$! 
 
-renice +10 -p $pid0
+renice +10 -p $pid1
+
+at now + 1min <<< "kill ${pid3}"
+at now + 2min <<< "kill ${pid1}; kill ${pid2}"
