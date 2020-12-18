1ce4bb96c22f2191529aa3463fbea35fda78efc0
ew0s <ewos365@mail.ru>
2020-12-18 14:49:40 +0300

    Add: 6 task for 3 lab

--- lab3-main/task5/generator.sh
+++ lab3-main/task5/generator.sh
@@ -11,7 +11,7 @@ do
         exit 0
     fi
 
-    if [[ "$line" != "+" && "$line" != "*" && "$line" != [0-9] ]];
+    if [[ $line != "+" && $line != "*" && "$line" != [0-9] ]];
     then
         echo "[Generator]: Error - no such commnad"
         exit 1
