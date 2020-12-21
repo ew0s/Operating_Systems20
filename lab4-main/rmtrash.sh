ce833fc2ecd31c53cb198d939631706cf031747c
ew0s <ewos365@mail.ru>
2020-12-21 13:38:33 +0300

    Add: rmtrash for 4 main lab

---
+++ lab4-main/rmtrash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $# > 1 ]];
+then
+    echo "[Error]: Too many arguments"
+    exit 1
+fi
+
+if [[ $# < 1 ]];
+then
+    echo "[Error]: Too few arguments"
+    exit 1
+fi
+
+if [[ ! -f !$1]];
+then
+    echo "[Error]: given file not a file"
+    exit 1
+fi
+
+if [[ ! -e "/home/user/.trash" ]];
+then
+    mkdir /home/user/.trash
+fi
+
+value=$(find )
