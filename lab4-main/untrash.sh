cd588223f24ebf456e095bad88c3a687b70cedca
ew0s <ewos365@mail.ru>
2020-12-23 17:57:15 +0300

    Add: untrash script for 4 main lab

--- lab4-main/untrash.sh
+++ lab4-main/untrash.sh
@@ -2,12 +2,54 @@
 
 if [[ $# > 1 || $# < 1 ]];
 then
-    echo "Wrong arguments count"
+    echo "[ERROR]: Wrong arguments count"
     exit 1
 fi
 
-[ -f "~/trash.log" ] || { echo "File trash.log does not exist"}; exit 1; };
-[ -s "~/trash.log" ] || { echo "File trash.log is empty"; exit 1; };
+if [[ $1 == *"/"* ]]
+then
+    echo "[ERROR]: Argument can not be a file path";
+    exit 1
+fi
 
-[ -d "~/trash" ] || { echo "Directory trash does not exist"; exit 1; };
-[]
+TrashLogPath=$~/trash.log
+TrashPath=$~/trash
+
+[ -f $TrashLogPath ] || { echo "[ERROR]: Hidden file trash.log does not exist"}; exit 1; };
+[ -s $TrashLogPath ] || { echo "[ERROR]: Hidden file trash.log is empty"; exit 1; };
+
+[ -d $TrashPath ] || { echo "[ERROR]: Hidden directory trash does not exist"; exit 1; };
+
+FileCount=$(echo $( ls -A $TrashPath | wc -l ))
+[ FileCount -e 0 ] && { echo "[ERROR]: Hidden directory is empty"; exit 1; };
+
+
+function Recover()
+{
+
+}
+
+Matches=$(mktemp /tmp/untrash_script.XXXXXX)
+grep -i "$1" "$TrashLogPath" > Matches
+
+if [[ ! -s Matches ]]
+then 
+    echo "[ERROR]: Unable to find such file in trash directory"
+    exit 1
+fi
+
+cat Matches | while read LINE
+do
+    FilePath=$(echo "$LINE" | awk '{print $1}')
+    LinkName=$(echo "$LINE" | awk '{print $2}')
+
+    echo "Press ENTER to recover $FilePath from trash:"
+    echo "Press any other key to continue"
+    read input < /dev/tty
+    if [[ $input == "" ]]
+    then
+        recover $FilePath $LinkName $1 $TrashPath $TrashLogPath
+    else
+        continue
+    fi
+done
