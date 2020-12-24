bebcb6d74fefdcdaf2f27a1e7cb1ba3ee3dba40b
ew0s <ewos365@mail.ru>
2020-12-24 12:58:50 +0300

    Add: upback.sh in 4 main lab

---
+++ lab4-main/upback.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LastBackupDate=$(find ~ -name "Backup*" | awk 'BEGIN{FS="-"}{print $2"-"$3"-"$4}' | sort -nr | head -1)
+LastBackup=~/Backup-$LastBackupDate
+
+if [[ -z "$LastBackup" ]]
+then
+    echo "Backup directory doesn't exist"
+	exit 1
+fi 
+
+if [[ ! -d ~/restore ]]
+then
+	mkdir ~/restore
+else
+	rm -r ~/restore
+	mkdir ~/restore
+fi
+
+for file in $(ls $lastBackup | grep -Ev "\.[0-9]{4}-[0-9]{2}-[0-9]{2}$")
+do
+	cp $lastBackup/$file ~/restore/$file
+done
