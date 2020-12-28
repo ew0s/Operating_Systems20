302b3ef2c5138763f9191bdb894be3342395be44
ew0s <ewos365@mail.ru>
2020-12-28 09:56:18 +0300

    Fix: new backup for 4 main lab

--- lab4-main/backup_updated.sh
+++ lab4-main/backup_updated.sh
@@ -8,11 +8,15 @@ if [[ ! -f "~/backup-report" ]]; then
     touch ~/backup-report
 fi
 
-echo $LastUpdateDate
-echo $NewDirectory
-
 if [[ -z $LastUpdateDate ]]; then
     mkdir $NewDirectory
     cp -a ~/source/. $NewDirectory
-    echo -e "[$CurrentDate] Backup has created successfully.\nNew values:\n $(ls $NewDirectory)" >> ~/backup-report
+    echo -e "[$CurrentDate] Backup has created successfully.\nNew values:\n$(ls $NewDirectory)" >> ~/backup-report
+    exit 0
 fi
+
+LastBackup=~/Backup-$LastUpdateDate
+LastBackupSeconds=$(date -d $LastUpdateDate +"%s")
+CurrentSeconds=$(date -d $CurrentDate +"%s")
+DayDifference=$(echo "($CurrentSeconds - $LastBackupSeconds) / 60 / 60 / 24")
+echo $DayDifference
