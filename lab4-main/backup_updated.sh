8f89e4cfa77147645e3892855f735b399aeb2d73
ew0s <ewos365@mail.ru>
2020-12-28 10:22:12 +0300

    Fix: new backup for 4 main lab

--- lab4-main/backup_updated.sh
+++ lab4-main/backup_updated.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+function CreateNewBackup
+{
+    local NewDirectory=$1
+    local CurrentDate=$2
+
+    mkdir $NewDirectory
+    cp -a ~/source/. $NewDirectory
+    echo -e "[$CurrentDate] Backup has created successfully.\nNew values:\n$(ls $NewDirectory)" >> ~/backup-report
+}
+
+function CopyFile
+{
+    local File=$1
+    local LastBackupDirectory=$2
+    local NewFilesLog=$3
+
+    cp ~/source/$File $LastBackupDirectory
+    NewFilesLog="$NewFilesLog\nNew file: $File"
+
+    return $NewFilesLog
+}
+
 CurrentDate=$(date +"%Y-%m-%d")
 NewDirectory=~/Backup-$CurrentDate
 LastUpdateDate=$(find ~ -name "Backup*" | awk 'BEGIN{FS="-"}{print $2"-"$3"-"$4}' | sort -nr | head -1)
@@ -9,14 +31,24 @@ if [[ ! -f "~/backup-report" ]]; then
 fi
 
 if [[ -z $LastUpdateDate ]]; then
-    mkdir $NewDirectory
-    cp -a ~/source/. $NewDirectory
-    echo -e "[$CurrentDate] Backup has created successfully.\nNew values:\n$(ls $NewDirectory)" >> ~/backup-report
+    CreateNewBackup $NewDirectory $CurrentDate 
     exit 0
 fi
 
-LastBackup=~/Backup-$LastUpdateDate
+LastBackupDirectory=~/Backup-$LastUpdateDate
 LastBackupSeconds=$(date -d $LastUpdateDate +"%s")
 CurrentSeconds=$(date -d $CurrentDate +"%s")
 DayDifference=$(echo "($CurrentSeconds - $LastBackupSeconds) / 60 / 60 / 24" | bc)
-echo $DayDifference
+
+if [[ $DayDifference -gt 6 ]]; then
+    CreateNewBackup $NewDirectory $CurrentDate
+    exit 0
+fi
+
+for file in $(ls ~/source)
+do
+    if [[ ! -f $LastBackupDirectory/$file ]]; then
+        NewFilesLog=$( CopyFile $file $LastBackupDirectory $NewFilesLog)
+    fi
+done
+
