1c2e60367b5d46bc4e7d01372beec69d0f01c16e
ew0s <ewos365@mail.ru>
2020-12-28 09:19:08 +0300

    Add: new backup for 4 main lab

---
+++ lab4-main/backup_updated.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CurrentDate=$(date +"%Y-%m-%d")
+NewDirectory=~/Backup-$date
+LastUpdateDate=$(find ~ -name "Backup*" | awk 'BEGIN{FS="-"}{print $2"-"$3"-"$4}' | sort -nr | head -1)
+
+if [[ ! -f "~/backup-report" ]]; then
+    touch ~/backup-report
+fi
+
+echo $LastUpdateDate
+
+if [[ -z $LastUpdateDate ]]; then
+    mkdir $NewDirectory
+    cp -a ~/source/. $NewDirectory
+    echo -e "[$Date] Backup has created successfully.\n\tNew values: $(ls $NewDirectory)" >> ~/backup-report
+fi
